07a460519627641dfe2bf3f4d70d3089223d24dd
Leixb <abone9999@gmail.com>
2020-06-15 22:59:04 +0200

    Example of get_prop

---
+++ examples/properties.rs
@@ -0,0 +1,26 @@
+extern crate yeelight;
+
+use yeelight::*;
+
+fn main() {
+    let my_bulb_ip = "192.168.1.204";
+    let bulb = Bulb::new(my_bulb_ip, 55443);
+
+    // Turn on the bulb
+    bulb.set_power(Power::On, Effect::Sudden, 0, Mode::Normal)
+        .expect("Failed to communicate with bulb (set_power)");
+
+    // Define flow array
+    let props = Properties::new(vec![
+        Property::Power,
+        Property::Bright,
+        Property::CT,
+        Property::RGB,
+    ]);
+    // Send flow command
+    println!(
+        "Response: {}",
+        bulb.get_prop(props)
+            .expect("Failed to communicate with bulb (get_prop)")
+    );
+}
