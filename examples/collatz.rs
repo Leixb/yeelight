4f9f90fab4e6e3cab42047600803fce79c026e04
Leixb <abone9999@gmail.com>
2020-06-25 23:33:46 +0200

    Started fixing examples

--- examples/collatz.rs
+++ examples/collatz.rs
@@ -5,13 +5,14 @@ use yeelight::*;
 // This program is meant to demonstrate some examples of commands and how to read the results turns
 // on the bulb, changes the brightness following the collatz sequence (mod 100) 10 times waiting 1
 // second each, and then sets the color to red over 10 seconds.
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut bulb = Bulb::connect("192.168.1.204", 55443)?;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut bulb = Bulb::connect("192.168.1.204", 55443).await?;
 
     // Turn on the bulb
     println!(
         "Response: {:?}",
-        bulb.set_power(Power::On, Effect::Sudden, 0, Mode::Normal)?
+        bulb.set_power(Power::On, Effect::Sudden, 0, Mode::Normal).await?
     );
 
     // Define flow array
@@ -24,8 +25,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let second = time::Duration::from_millis(1000);
 
-    for _ in 1..10 {
-        let response = bulb.get_prop(&props)?;
+    for _ in 1..10u8 {
+        let response = bulb.get_prop(&props).await?;
         let brightness = match response {
             Response::Result(result) => {
                 println!("Properties: {:?}", result);
@@ -49,14 +50,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("Setting brightness to {}", brightness);
 
         // Change brightness
-        let response = bulb.set_bright(brightness, Effect::Smooth, 1000)?;
+        let response = bulb.set_bright(brightness, Effect::Smooth, 1000).await?;
         eprintln!("Response: {:?}", response);
 
         thread::sleep(second);
     }
 
     // Set bulb to pure red over 10 seconds
-    let response = bulb.set_rgb(0xff_00_00, Effect::Smooth, 10000)?;
+    let response = bulb.set_rgb(0xff_00_00, Effect::Smooth, 10000).await?;
     if let Response::Error(code, message) = response {
         eprintln!("Error (code {}): {}", code, message);
         std::process::exit(code);
