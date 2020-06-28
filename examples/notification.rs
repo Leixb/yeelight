9ef852bad7049ed533d0fd459927d69a2ff478ce
Leixb <abone9999@gmail.com>
2020-06-28 23:16:36 +0200

    Add notification example

---
+++ examples/notification.rs
@@ -0,0 +1,21 @@
+use yeelight::Bulb;
+use yeelight::Notification;
+
+use tokio::sync::mpsc;
+
+#[tokio::main]
+async fn main() {
+    let bulb = Bulb::connect("192.168.1.204", 55443).await.unwrap();
+    let mut bulb = bulb.no_response();
+
+    let (sender, mut recv) = mpsc::channel(10);
+
+    bulb.set_notify(sender).await;
+
+    while let Some(Notification(i)) = recv.recv().await {
+        for (k, v) in i.iter() {
+            println!("{} {}", k, v);
+        }
+    }
+
+}
