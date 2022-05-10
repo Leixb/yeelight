0e1537ea3eeb05c495bafabbed49c3f005421fc3
LeixB <abone9999@gmail.com>
2022-05-11 00:07:39 +0200

    Simplify properties example

--- examples/properties.rs
+++ examples/properties.rs
@@ -10,31 +10,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let my_bulb_ip = "192.168.1.204";
     let mut bulb = Bulb::connect(my_bulb_ip, 55443).await?;
 
-    // Turn on the bulb
-    println!(
-        "Response: {:?}",
-        bulb.set_power(
-            Power::On,
-            Effect::Sudden,
-            Duration::from_millis(0),
-            Mode::Normal
-        )
-        .await?
-    );
-
-    // Define flow array
+    // Define properties to query
     let props = Properties(vec![
         Property::Power,
         Property::Bright,
         Property::CT,
         Property::RGB,
     ]);
-    // Send flow command
+    // Print response with properties
     println!("Response: {:?}", bulb.get_prop(&props).await?);
-    println!(
-        "Response: {:?}",
-        bulb.set_rgb(122, Effect::Smooth, Duration::from_millis(500))
-            .await?
-    );
     Ok(())
 }
