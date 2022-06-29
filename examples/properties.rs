f216de8d84c787023f706442ebca1b51aa81dff8
LeixB <abone9999@gmail.com>
2022-06-29 16:25:08 +0200

    Fix example

--- examples/properties.rs
+++ examples/properties.rs
@@ -1,6 +1,6 @@
-use std::time::Duration;
 
-use yeelight::{Bulb, Effect, Mode, Power, Properties, Property};
+
+use yeelight::{Bulb, Properties, Property};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
