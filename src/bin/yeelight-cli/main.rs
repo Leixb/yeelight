19fb52e1e39dd7485d39f83ba72ade1d1df36fbd
LeixB <abone9999@gmail.com>
2022-06-29 17:16:14 +0200

    Add cli into crate
    All the code from the yeelight-cli crate now resides in this crate.
    This should ease usability and removes unnecessary fragmentation.

---
+++ src/bin/yeelight-cli/main.rs
@@ -0,0 +1,294 @@
+mod presets;
+
+use std::time::Duration;
+
+use structopt::{
+    clap::{AppSettings, ArgGroup},
+    StructOpt,
+};
+
+use tokio::sync::mpsc;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "yeelight-cli",
+    about = "A CLI to control your Yeelight smart lights."
+)]
+#[structopt(global_setting = AppSettings::ColoredHelp)]
+struct Options {
+    #[structopt(env = "YEELIGHT_ADDR")]
+    address: String,
+    #[structopt(short, long, default_value = "55443", env = "YEELIGHT_PORT")]
+    port: u16,
+    #[structopt(subcommand)]
+    subcommand: Command,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    #[structopt(about = "Get properties")]
+    Get {
+        #[structopt(possible_values = &yeelight::Property::variants(), case_insensitive = true)]
+        #[structopt(required = true)]
+        properties: Vec<yeelight::Property>,
+    },
+    #[structopt(about = "Toggle light")]
+    #[structopt(group = ArgGroup::with_name("light"))]
+    Toggle {
+        #[structopt(long, group = "light")]
+        dev: bool,
+        #[structopt(long, group = "light")]
+        bg: bool,
+    },
+    #[structopt(about = "Turn on light")]
+    On {
+        #[structopt(possible_values = &yeelight::Effect::variants(), case_insensitive = true)]
+        #[structopt(short, long, default_value = "Smooth")]
+        effect: yeelight::Effect,
+        #[structopt(short, long, default_value = "500")]
+        duration: u64,
+        #[structopt(possible_values = &yeelight::Mode::variants(), case_insensitive = true)]
+        #[structopt(short, long, default_value = "Normal")]
+        mode: yeelight::Mode,
+        #[structopt(long)]
+        bg: bool,
+    },
+    #[structopt(about = "Turn off light")]
+    Off {
+        #[structopt(possible_values = &yeelight::Effect::variants(), case_insensitive = true)]
+        #[structopt(short, long, default_value = "Smooth")]
+        effect: yeelight::Effect,
+        #[structopt(short, long, default_value = "500")]
+        duration: u64,
+        #[structopt(possible_values = &yeelight::Mode::variants(), case_insensitive = true)]
+        #[structopt(short, long, default_value = "Normal")]
+        mode: yeelight::Mode,
+        #[structopt(long)]
+        bg: bool,
+    },
+    #[structopt(about = "Start timer")]
+    Timer { minutes: u64 },
+    #[structopt(about = "Clear current timer")]
+    TimerClear,
+    #[structopt(about = "Get remaining minutes for timer")]
+    TimerGet,
+    #[structopt(about = "Set values")]
+    Set {
+        #[structopt(flatten)]
+        property: Prop,
+        #[structopt(possible_values = &yeelight::Effect::variants(), case_insensitive = true)]
+        #[structopt(short, long, default_value = "Smooth")]
+        effect: yeelight::Effect,
+        #[structopt(short, long, default_value = "500")]
+        duration: u64,
+    },
+    #[structopt(about = "Start color flow")]
+    Flow {
+        expression: yeelight::FlowExpresion,
+        #[structopt(default_value = "0")]
+        count: u8,
+        #[structopt(possible_values = &yeelight::CfAction::variants(), case_insensitive = true)]
+        #[structopt(default_value = "Recover")]
+        action: yeelight::CfAction,
+        #[structopt(long)]
+        bg: bool,
+    },
+    #[structopt(about = "Stop color flow")]
+    FlowStop {
+        #[structopt(long)]
+        bg: bool,
+    },
+    #[structopt(about = "Adjust properties (Bright/CT/Color) (increase/decrease/circle)")]
+    Adjust {
+        #[structopt(possible_values = &yeelight::Prop::variants(), case_insensitive = true)]
+        property: yeelight::Prop,
+        #[structopt(possible_values = &yeelight::AdjustAction::variants(), case_insensitive = true)]
+        action: yeelight::AdjustAction,
+        #[structopt(long)]
+        bg: bool,
+    },
+    #[structopt(about = "Adjust properties (Bright/CT/Color) with perentage (-100~100)")]
+    #[structopt(setting = AppSettings::AllowNegativeNumbers)]
+    AdjustPercent {
+        #[structopt(possible_values = &yeelight::Prop::variants(), case_insensitive = true)]
+        property: yeelight::Prop,
+        percent: i8,
+        #[structopt(default_value = "500")]
+        duration: u64,
+        #[structopt(long)]
+        bg: bool,
+    },
+    #[structopt(about = "Connect to music TCP stream")]
+    MusicConnect { host: String, port: u16 },
+    #[structopt(about = "Stop music mode")]
+    MusicStop,
+    #[structopt(about = "Presets")]
+    Preset {
+        #[structopt(possible_values = &presets::Preset::variants(), case_insensitive = true)]
+        preset: presets::Preset,
+    },
+    #[structopt(about = "Listen to notifications from lamp")]
+    Listen,
+}
+
+#[derive(Debug, StructOpt)]
+enum Prop {
+    Power {
+        #[structopt(possible_values = &yeelight::Power::variants(), case_insensitive = true)]
+        power: yeelight::Power,
+        #[structopt(possible_values = &yeelight::Mode::variants(), case_insensitive = true)]
+        #[structopt(default_value = "Normal")]
+        mode: yeelight::Mode,
+        #[structopt(long)]
+        bg: bool,
+    },
+    CT {
+        color_temperature: u16,
+        #[structopt(long)]
+        bg: bool,
+    },
+    RGB {
+        rgb_value: u32,
+        #[structopt(long)]
+        bg: bool,
+    },
+    HSV {
+        hue: u16,
+        #[structopt(default_value = "100")]
+        sat: u8,
+        #[structopt(long)]
+        bg: bool,
+    },
+    Bright {
+        brightness: u8,
+        #[structopt(long)]
+        bg: bool,
+    },
+    Name {
+        name: String,
+    },
+    Scene {
+        #[structopt(possible_values = &yeelight::Class::variants(), case_insensitive = true)]
+        class: yeelight::Class,
+        val1: u64,
+        #[structopt(default_value = "100")]
+        val2: u64,
+        #[structopt(default_value = "100")]
+        val3: u64,
+        #[structopt(long)]
+        bg: bool,
+    },
+    Default {
+        #[structopt(long)]
+        bg: bool,
+    },
+}
+
+macro_rules! sel_bg {
+    ($obj:tt.$fn:ident ($($p:expr),*) || $fn_bg:ident if $var:tt ) => (
+        if $var {
+            $obj.$fn_bg($($p),*).await
+        } else {
+            $obj.$fn($($p),*).await
+        }
+    );
+}
+
+#[tokio::main]
+async fn main() {
+    let opt = Options::from_args();
+
+    let mut bulb = yeelight::Bulb::connect(&opt.address, opt.port)
+        .await
+        .unwrap();
+
+    let response = match opt.subcommand {
+        Command::Toggle{bg, dev} => {
+            match (bg, dev) {
+                (true, _) => bulb.bg_toggle().await,
+                (_, true) => bulb.dev_toggle().await,
+                _ => bulb.toggle().await,
+            }
+        },
+        Command::On {
+            effect,
+            duration,
+            mode,
+            bg,
+        } => sel_bg!(bulb.set_power(yeelight::Power::On, effect, Duration::from_millis(duration), mode) || bg_set_power if bg),
+        Command::Off {
+            effect,
+            duration,
+            mode,
+            bg,
+        } => sel_bg!(bulb.set_power(yeelight::Power::Off, effect, Duration::from_millis(duration), mode) || bg_set_power if bg),
+        Command::Get { properties } => bulb.get_prop(&yeelight::Properties(properties)).await,
+        Command::Set {
+            property,
+            effect,
+            duration,
+        } => match property {
+            Prop::Power { power, mode, bg} => sel_bg!(bulb.set_power(power, effect, Duration::from_millis(duration), mode) || bg_set_power if bg),
+            Prop::CT { color_temperature, bg} => sel_bg!(bulb.set_ct_abx(color_temperature, effect, Duration::from_millis(duration)) || bg_set_ct_abx if bg),
+            Prop::RGB { rgb_value, bg} => sel_bg!(bulb.set_rgb(rgb_value, effect, Duration::from_millis(duration)) || bg_set_rgb if bg),
+            Prop::HSV { hue, sat, bg} => sel_bg!(bulb.set_hsv(hue, sat, effect, Duration::from_millis(duration)) || bg_set_hsv if bg),
+            Prop::Bright { brightness, bg} => sel_bg!(bulb.set_bright(brightness, effect, Duration::from_millis(duration)) || bg_set_bright if bg),
+            Prop::Name { name } => bulb.set_name(&name).await,
+            Prop::Scene {
+                class,
+                val1,
+                val2,
+                val3,
+                bg,
+            } => sel_bg!(bulb.set_scene(class, val1, val2, val3) || bg_set_scene if bg),
+            Prop::Default{bg} => sel_bg!(bulb.set_default() || bg_set_default if bg),
+        },
+        Command::Timer { minutes } => bulb.cron_add(yeelight::CronType::Off, minutes).await,
+        Command::TimerClear => bulb.cron_del(yeelight::CronType::Off).await,
+        Command::TimerGet => bulb.cron_get(yeelight::CronType::Off).await,
+        Command::Flow {
+            count,
+            action,
+            expression,
+            bg,
+        } => sel_bg!(bulb.start_cf(count, action, expression) || bg_start_cf if bg),
+        Command::FlowStop { bg } => sel_bg!(bulb.stop_cf() || bg_stop_cf if bg),
+        Command::Adjust { action, property, bg } => sel_bg!(bulb.set_adjust(action, property) || bg_set_adjust if bg),
+        Command::AdjustPercent {
+            property,
+            percent,
+            duration,
+            bg,
+        } => match property {
+            yeelight::Prop::Bright => sel_bg!(bulb.adjust_bright(percent, Duration::from_millis(duration)) || bg_adjust_bright if bg),
+            yeelight::Prop::Color => sel_bg!(bulb.adjust_color(percent, Duration::from_millis(duration)) || bg_adjust_color if bg),
+            yeelight::Prop::CT => sel_bg!(bulb.adjust_ct(percent, Duration::from_millis(duration)) || bg_adjust_ct if bg),
+        },
+        Command::MusicConnect { host, port } => {
+            bulb.set_music(yeelight::MusicAction::On, &host, port).await
+        }
+        Command::MusicStop => bulb.set_music(yeelight::MusicAction::Off, &"".to_string(), 0).await,
+        Command::Preset{ preset } => presets::apply(bulb, preset).await,
+        Command::Listen => {
+            let (sender, mut recv) = mpsc::channel(10);
+
+            bulb.set_notify(sender).await;
+
+            while let Some(yeelight::Notification(i)) = recv.recv().await {
+                for (k, v) in i.iter() {
+                    println!("{} {}", k, v);
+                }
+            }
+            Ok(None)
+        }
+    }
+    .unwrap();
+
+    if let Some(result) = response {
+        result.iter().for_each(|x| {
+            if x != "ok" {
+                println!("{}", x)
+            }
+        });
+    }
+}
---
+++ src/bin/yeelight-cli/presets.rs
@@ -0,0 +1,256 @@
+use yeelight::{BulbError, FlowExpresion, FlowTuple, Response};
+
+use std::time::Duration;
+
+use structopt::clap::arg_enum;
+
+arg_enum! {
+    #[derive(Debug)]
+    pub enum Preset {
+        Candle,
+        Reading,
+        NightReading,
+        CosyHome,
+        Romantic,
+        Birthday,
+        DateNight,
+        Teatime,
+        PcMode,
+        Concentration,
+        Movie,
+        Night,
+        Notify,
+        Notify2,
+
+        PulseRed,
+        PulseBlue,
+        PulseGreen,
+
+        Red,
+        Green,
+        Blue,
+
+        Police,
+        Police2,
+        Disco,
+        Temp,
+    }
+}
+
+enum PresetValue {
+    RGB(u32, u8),
+    HSV(u16, u8, u8),
+    CT(u16, u8),
+    Flow(yeelight::FlowExpresion, u8, yeelight::CfAction),
+}
+
+pub async fn apply(bulb: yeelight::Bulb, preset: Preset) -> Result<Option<Response>, BulbError> {
+    use Preset::*;
+    let red = 0xFF_00_00;
+    let green = 0x00_FF_00;
+    let blue = 0x00_00_FF;
+    match preset {
+        Candle => send(bulb, candle()).await,
+        Reading => send(bulb, reading()).await,
+        NightReading => send(bulb, night_reading()).await,
+        CosyHome => send(bulb, cosy_home()).await,
+        Romantic => send(bulb, romantic()).await,
+        Birthday => send(bulb, birthday()).await,
+        DateNight => send(bulb, date_night()).await,
+        Teatime => send(bulb, teatime()).await,
+        PcMode => send(bulb, pc_mode()).await,
+        Concentration => send(bulb, concentration()).await,
+        Movie => send(bulb, movie()).await,
+        Night => send(bulb, night()).await,
+        Notify => send(bulb, notify()).await,
+        Notify2 => send(bulb, notify2()).await,
+
+        Red => send(bulb, PresetValue::RGB(red, 100)).await,
+        Green => send(bulb, PresetValue::RGB(green, 100)).await,
+        Blue => send(bulb, PresetValue::RGB(blue, 100)).await,
+
+        PulseRed => send(bulb, pulse(red, 100, 250)).await,
+        PulseGreen => send(bulb, pulse(green, 100, 250)).await,
+        PulseBlue => send(bulb, pulse(blue, 100, 250)).await,
+        Police => send(bulb, police(100)).await,
+        Police2 => send(bulb, police2(100)).await,
+        Disco => send(bulb, disco(120)).await,
+        Temp => send(bulb, temp(2600, 5000, 100)).await,
+    }
+}
+
+async fn send(
+    mut bulb: yeelight::Bulb,
+    preset: PresetValue,
+) -> Result<Option<Response>, BulbError> {
+    match preset {
+        PresetValue::Flow(expression, count, action) => {
+            bulb.start_cf(count, action, expression).await
+        }
+        PresetValue::RGB(color, bright) => {
+            bulb.set_scene(yeelight::Class::Color, color.into(), bright.into(), 0)
+                .await
+        }
+        PresetValue::HSV(hue, sat, bright) => {
+            bulb.set_scene(yeelight::Class::HSV, hue.into(), sat.into(), bright.into())
+                .await
+        }
+        PresetValue::CT(ct, bright) => {
+            bulb.set_scene(yeelight::Class::CT, ct.into(), bright.into(), 0)
+                .await
+        }
+    }
+}
+
+fn disco(bpm: u64) -> PresetValue {
+    let duration = Duration::from_millis(1000/ bpm);
+    let expr = FlowExpresion(vec![
+        FlowTuple::rgb(duration, 0xFF_00_00, 100),
+        FlowTuple::rgb(duration, 0xFF_00_00, 1),
+        FlowTuple::rgb(duration, 0x80_FF_00, 100),
+        FlowTuple::rgb(duration, 0x80_FF_00, 1),
+        FlowTuple::rgb(duration, 0x00_FF_FF, 100),
+        FlowTuple::rgb(duration, 0x00_FF_FF, 1),
+        FlowTuple::rgb(duration, 0x80_00_FF, 100),
+        FlowTuple::rgb(duration, 0x80_00_FF, 1),
+    ]);
+    PresetValue::Flow(expr, 0, yeelight::CfAction::Stay)
+}
+
+fn temp(a: u32, b: u32, brightness: i8) -> PresetValue {
+    let duration = Duration::from_millis(40_000);
+    let expr = FlowExpresion(vec![
+        FlowTuple::ct(duration, a, brightness),
+        FlowTuple::ct(duration, b, brightness),
+    ]);
+    PresetValue::Flow(expr, 0, yeelight::CfAction::Stay)
+}
+
+fn pulse(rgb: u32, brightness: i8, duration: u64) -> PresetValue {
+    let duration = Duration::from_millis(duration);
+    let expr = FlowExpresion(vec![
+        FlowTuple::rgb(duration, rgb, brightness),
+        FlowTuple::rgb(duration, rgb, 1),
+    ]);
+    PresetValue::Flow(expr, 2, yeelight::CfAction::Recover)
+}
+
+fn police(brightness: i8) -> PresetValue {
+    let duration = Duration::from_millis(300);
+    let (red, blue) = (0xFF_00_00, 0x00_00_FF);
+    let expr = FlowExpresion(vec![
+        FlowTuple::rgb(duration, red, brightness),
+        FlowTuple::rgb(duration, blue, brightness),
+    ]);
+    PresetValue::Flow(expr, 0, yeelight::CfAction::Stay)
+}
+
+fn police2(brightness: i8) -> PresetValue {
+    let duration = Duration::from_millis(300);
+    let (red, blue) = (0xFF_00_00, 0x00_00_FF);
+    let expr = FlowExpresion(vec![
+        FlowTuple::rgb(duration, red, brightness),
+        FlowTuple::rgb(duration, red, 1),
+        FlowTuple::rgb(duration, red, brightness),
+        FlowTuple::sleep(duration),
+        FlowTuple::rgb(duration, blue, brightness),
+        FlowTuple::rgb(duration, blue, 1),
+        FlowTuple::rgb(duration, blue, brightness),
+        FlowTuple::sleep(duration),
+    ]);
+    PresetValue::Flow(expr, 0, yeelight::CfAction::Stay)
+}
+
+fn candle() -> PresetValue {
+    let ct = 2700;
+    let expr = FlowExpresion(vec![
+        FlowTuple::ct(Duration::from_millis(800), ct, 50),
+        FlowTuple::ct(Duration::from_millis(800), ct, 30),
+        FlowTuple::ct(Duration::from_millis(1200), ct, 80),
+        FlowTuple::ct(Duration::from_millis(800), ct, 60),
+        FlowTuple::ct(Duration::from_millis(1200), ct, 90),
+        FlowTuple::ct(Duration::from_millis(2400), ct, 50),
+        FlowTuple::ct(Duration::from_millis(1200), ct, 80),
+        FlowTuple::ct(Duration::from_millis(800), ct, 60),
+        FlowTuple::ct(Duration::from_millis(400), ct, 70),
+    ]);
+    PresetValue::Flow(expr, 0, yeelight::CfAction::Stay)
+}
+fn reading() -> PresetValue {
+    PresetValue::CT(3500, 100)
+}
+fn night_reading() -> PresetValue {
+    PresetValue::CT(4000, 40)
+}
+
+fn cosy_home() -> PresetValue {
+    PresetValue::CT(2700, 80)
+}
+
+fn romantic() -> PresetValue {
+    let expr = FlowExpresion(vec![
+        FlowTuple::rgb(Duration::from_millis(4000), 0x59_15_6D, 1),
+        FlowTuple::rgb(Duration::from_millis(4000), 0x66_14_2A, 1),
+    ]);
+    PresetValue::Flow(expr, 0, yeelight::CfAction::Stay)
+}
+
+fn birthday() -> PresetValue {
+    let expr = FlowExpresion(vec![
+        FlowTuple::rgb(Duration::from_millis(1996), 0xDC_50_19, 80),
+        FlowTuple::rgb(Duration::from_millis(1996), 0xDC_78_1E, 80),
+        FlowTuple::rgb(Duration::from_millis(1996), 0xAA_32_14, 80),
+    ]);
+    PresetValue::Flow(expr, 0, yeelight::CfAction::Stay)
+}
+
+fn date_night() -> PresetValue {
+    PresetValue::HSV(24, 100, 50)
+}
+
+fn teatime() -> PresetValue {
+    PresetValue::CT(3000, 50)
+}
+
+fn pc_mode() -> PresetValue {
+    PresetValue::CT(2700, 30)
+}
+fn concentration() -> PresetValue {
+    PresetValue::CT(5000, 100)
+}
+
+fn movie() -> PresetValue {
+    PresetValue::HSV(240, 60, 50)
+}
+
+fn night() -> PresetValue {
+    PresetValue::HSV(36, 100, 1)
+}
+
+fn notify() -> PresetValue {
+    let duration = Duration::from_millis(300);
+    let temp = 5000;
+    let expr = FlowExpresion(vec![
+        FlowTuple::ct(duration, temp, 100),
+        FlowTuple::ct(duration, temp, 1),
+        FlowTuple::ct(duration, temp, 100),
+        FlowTuple::ct(duration, temp, 1),
+        FlowTuple::ct(duration, temp, 100),
+        FlowTuple::ct(duration, temp, 1),
+    ]);
+    let len = &expr.0.len();
+    PresetValue::Flow(expr, *len as u8, yeelight::CfAction::Recover)
+}
+
+fn notify2() -> PresetValue {
+    let duration = Duration::from_millis(200);
+    let temp = 5000;
+    let expr = FlowExpresion(vec![
+        FlowTuple::ct(duration, temp, 100),
+        FlowTuple::ct(duration, temp, 1),
+        FlowTuple::ct(duration, temp, 100),
+        FlowTuple::ct(duration, temp, 1),
+    ]);
+    let len = &expr.0.len();
+    PresetValue::Flow(expr, *len as u8, yeelight::CfAction::Recover)
+}
