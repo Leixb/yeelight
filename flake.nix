e663178e8cc1cf589996606e74df0d9c9e6fddff
LeixB <abone9999@gmail.com>
2022-07-03 17:34:53 +0200

    Use flake-parts and enable git-hooks

--- flake.nix
+++ flake.nix
@@ -3,16 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+
+    pre-commit-hooks.url = "github:hercules-ci/pre-commit-hooks.nix/flakeModule";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-parts, pre-commit-hooks }:
 
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
+    flake-parts.lib.mkFlake { inherit self; } {
+
+      imports = [
+        pre-commit-hooks.flakeModule
+      ];
+
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+
+        pre-commit.settings.hooks = {
+          nixpkgs-fmt.enable = true;
+          rustfmt.enable = true;
+        };
+
         devShells.default = pkgs.mkShell {
           name = "cargo";
           buildInputs = with pkgs; [
@@ -24,9 +38,13 @@
             rustfmt
             lldb
           ];
+
+          shellHook = ''
+            ${config.pre-commit.installationScript}
+          '';
         };
 
         packages.default = pkgs.callPackage ./default.nix { };
-      }
-    );
+      };
+    };
 }
