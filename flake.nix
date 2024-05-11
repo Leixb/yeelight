efaf29bb6556e220f7af49148ea5098a5120b3cb
Gleb Smirnov <glebsmirnov0708@gmail.com>
2024-05-12 01:39:54 +0300

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,16 @@
 {
-  description = "Cargo";
-
+  description = "Rust CLI and API bindings for yeelight WiFi Light Inter-Operation";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
 
     pre-commit-hooks.url = "github:hercules-ci/pre-commit-hooks.nix/flakeModule";
     pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-parts, pre-commit-hooks }:
+  outputs = { self, nixpkgs, flake-parts, pre-commit-hooks, ... }@inputs:
 
-    flake-parts.lib.mkFlake { inherit self; } {
+    flake-parts.lib.mkFlake { inherit inputs; } {
 
       imports = [
         pre-commit-hooks.flakeModule
@@ -34,6 +32,8 @@
             cargo
             cargo-edit
             cargo-release
+            cargo-outdated
+            clippy
             rust-analyzer
             rustc
             rustfmt
