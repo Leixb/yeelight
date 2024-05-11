7a0edc7be5ff0898b8ef61455aca8b6e846e8991
Gleb Smirnov <glebsmirnov0708@gmail.com>
2024-05-12 01:39:54 +0300

    Update flake

--- default.nix
+++ default.nix
@@ -3,13 +3,13 @@
 }:
 let
   toml = lib.importTOML ./Cargo.toml;
-in rustPlatform.buildRustPackage 
+in
+rustPlatform.buildRustPackage
 {
   pname = toml.package.name;
   version = toml.package.version;
 
   cargoLock.lockFile = ./Cargo.lock;
-  checkType = "lib";
   src = ./.;
 
   meta = with lib; {
